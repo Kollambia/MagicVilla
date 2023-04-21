bf178bc18fe7571132307c7d8c260e95aa1067e1
Ermek Abilov <ermek.abilove@gmail.com>
2023-04-21 19:25:40 +0600

    add http post method CreateVilla

--- MagicVilla_WebAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_WebAPI/Controllers/VillaAPIController.cs
@@ -1,9 +1,10 @@
 ﻿using MagicVilla_WebAPI.Controllers.DTO;
 using MagicVilla_WebAPI.Data;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Web.Http;
+using FromBodyAttribute = Microsoft.AspNetCore.Mvc.FromBodyAttribute;
 using HttpGetAttribute = Microsoft.AspNetCore.Mvc.HttpGetAttribute;
+using HttpPostAttribute = Microsoft.AspNetCore.Mvc.HttpPostAttribute;
 using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
 
 namespace MagicVilla_WebAPI.Controllers
@@ -34,6 +35,29 @@ namespace MagicVilla_WebAPI.Controllers
             var villa = VillaStore.VillaList.FirstOrDefault(u => u.Id == id);
 
             return villa != null ? Ok(villa) : NotFound("Такой виллы не существует");
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<VillaDTO> CreateVilla([FromBody]VillaDTO villa)
+        {
+            if(villa is null)
+            {
+                return BadRequest(villa);
+            }
+
+            if(villa.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            villa.Id = VillaStore.VillaList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
+            VillaStore.VillaList.Add(villa);
+
+            return Ok(villa);
+
         }
     }
 }   
