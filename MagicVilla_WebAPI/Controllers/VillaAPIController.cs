9f91741f35c84281b9474d1307be9785cd4a3a3f
Ermek Abilov <ermek.abilove@gmail.com>
2023-04-21 19:10:17 +0600

    Add first endpoint for the app

---
+++ MagicVilla_WebAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,39 @@
+﻿using MagicVilla_WebAPI.Controllers.DTO;
+using MagicVilla_WebAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Web.Http;
+using HttpGetAttribute = Microsoft.AspNetCore.Mvc.HttpGetAttribute;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+namespace MagicVilla_WebAPI.Controllers
+{
+
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
+        {
+            return Ok(VillaStore.VillaList);
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<VillaDTO> GetVilla(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+
+            var villa = VillaStore.VillaList.FirstOrDefault(u => u.Id == id);
+
+            return villa != null ? Ok(villa) : NotFound("Такой виллы не существует");
+        }
+    }
+}   
---
+++ MagicVilla_WebAPI/Data/VillaStore.cs
@@ -0,0 +1,13 @@
+﻿using MagicVilla_WebAPI.Controllers.DTO;
+
+namespace MagicVilla_WebAPI.Data
+{
+    public static class VillaStore
+    {
+        public static List<VillaDTO> VillaList = new List<VillaDTO>
+        {
+            new VillaDTO { Id =  1, Name = "Pool Villa" },
+            new VillaDTO { Id = 2, Name = "Beach Villa" }
+        };
+    }
+}
