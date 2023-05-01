2d4c44600505173dcd41308d1e496af07220aa18
Ermek Abilov <ermek.abilove@gmail.com>
2023-05-01 21:04:37 +0600

    some changes

--- MagicVilla_WebAPI/Controllers/DTO/VillaDTO.cs
+++ MagicVilla_WebAPI/Controllers/DTO/VillaDTO.cs
@@ -8,9 +8,10 @@ namespace MagicVilla_WebAPI.Controllers.DTO
         [Required]
         [MaxLength(30)]
         public string Name { get; set; }
-        public int Occupacity { get; set; }
         public string Details { get; set; }
+        [Required]
         public double Rate { get; set; }
+        public int Occupacity { get; set; }
         public int Sqft { get; set; }
         public string ImageUrl { get; set; }
         public string Amenity { get; set; }
